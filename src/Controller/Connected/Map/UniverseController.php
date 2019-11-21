ecf416979cbdd3c7e70677f358d815901fab9150
Matthieu Riviere <mriviere@hbs-research.com>
2019-11-21 16:54:55 +0100

    univers commander

--- src/Controller/Connected/Map/UniverseController.php
+++ src/Controller/Connected/Map/UniverseController.php
@@ -35,9 +35,42 @@ class UniverseController extends AbstractController
             ->getQuery()
             ->getResult();
 
+        $doms = $em->getRepository('App:Ally')
+            ->createQueryBuilder('a')
+            ->join('a.users', 'u')
+            ->join('u.planets', 'p')
+            ->select('a.id, a.sigle as alliance, count(p) as number')
+            ->groupBy('a.id')
+            ->orderBy('count(p.id)', 'DESC')
+            ->setMaxResults(3)
+            ->getQuery()
+            ->getResult();
+
+        $zombies = $em->getRepository('App:User')
+            ->createQueryBuilder('u')
+            ->join('u.planets', 'p')
+            ->select('u.id, count(p) as number')
+            ->groupBy('u.id')
+            ->where('g.position = :id')
+            ->orderBy('count(p.id)', 'DESC')
+            ->getQuery()
+            ->getOneOrNullResult();
+
+        $totalPlanet = $em->getRepository('App:Galaxy')
+            ->createQueryBuilder('g')
+            ->join('g.sectors', 's')
+            ->join('s.planets', 'p')
+            ->select('count(p) as number')
+            ->groupBy('g.id')
+            ->getQuery()
+            ->getSingleScalarResult();
+
         return $this->render('connected/map/universe.html.twig', [
             'galaxys' => $galaxys,
             'usePlanet' => $usePlanet,
+            'doms' => $doms,
+            'zombies' => $zombies,
+            'totalPlanet' => $totalPlanet
         ]);
     }
 }
