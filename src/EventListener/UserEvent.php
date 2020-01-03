4fc5edc51ecfcf5e15b2d4948a5135aaa3dd2977
Matthieu Rivière <mriviere@hbs-research.com>
2020-01-03 09:59:54 +0100

    fixbug alliance banque pdg et user event moon

--- src/EventListener/UserEvent.php
+++ src/EventListener/UserEvent.php
@@ -281,59 +281,97 @@ class UserEvent implements EventSubscriberInterface
                             $workerBonus = 1;
                         }
                         foreach ($user->getPlanets() as $planet) {
-                            $nbProd = ($planet->getNbProduction() * $seconds) / 60;
-                            $wtProd = ($planet->getWtProduction() * $seconds) / 60;
-                            $fdProd = ($planet->getFDProduction() * $seconds) / 60;
-                            $workerMore = (($planet->getWorkerProduction() * $workerBonus * $seconds) / 60);
+                            if ($planet->getRadar() == false and $planet->getSkyBrouilleur() == false and $planet->getMoon() == false) {
+                                $nbProd = ($planet->getNbProduction() * $seconds) / 60;
+                                $wtProd = ($planet->getWtProduction() * $seconds) / 60;
+                                $fdProd = ($planet->getFdProduction() * $seconds) / 60;
+                                $workerMore = (($planet->getWorkerProduction() * $workerBonus * $seconds) / 60);
 
-                            if ($seconds > 129600) {
-                                $secondsLvl = $seconds % 86400 + 1;
-                                $workerMore = $workerMore / $secondsLvl;
-                                if (($planet->getWorker() + $workerMore >= $planet->getWorkerMax())) {
-                                    $planet->setWorker($planet->getWorkerMax());
+                                if ($seconds > 129600) {
+                                    $secondsLvl = $seconds % 86400 + 1;
+                                    $workerMore = $workerMore / $secondsLvl;
+                                    if (($planet->getWorker() + $workerMore >= $planet->getWorkerMax())) {
+                                        $planet->setWorker($planet->getWorkerMax());
+                                    } else {
+                                        $planet->setWorker($planet->getWorker() + (($planet->getWorkerProduction() * $workerBonus * $seconds) / 60));
+                                    }
                                 } else {
-                                    $planet->setWorker($planet->getWorker() + (($planet->getWorkerProduction() * $workerBonus * $seconds) / 60));
+                                    if (($planet->getWorker() + $workerMore >= $planet->getWorkerMax())) {
+                                        $planet->setWorker($planet->getWorkerMax());
+                                    } else {
+                                        $planet->setWorker($planet->getWorker() + (($planet->getWorkerProduction() * $workerBonus * $seconds) / 60));
+                                    }
                                 }
-                            } else {
-                                if (($planet->getWorker() + $workerMore >= $planet->getWorkerMax())) {
-                                    $planet->setWorker($planet->getWorkerMax());
-                                } else {
-                                    $planet->setWorker($planet->getWorker() + (($planet->getWorkerProduction() * $workerBonus * $seconds) / 60));
-                                }
-                            }
 
-                            if ($user->getAlly()) {
-                                if ($user->getPoliticProd() > 0) {
-                                    $niobium = $planet->getNiobium() + ($nbProd * (1.2 + ($user->getPoliticProd() / 14)));
-                                    $water = $planet->getWater() + ($wtProd * (1.2 + ($user->getPoliticProd() / 14)));
-                                    $food = $planet->getFood() + ($fdProd * (1.2 + ($user->getPoliticProd() / 14)));
+                                if ($user->getAlly()) {
+                                    if ($user->getPoliticProd() > 0) {
+                                        $niobium = $planet->getNiobium() + ($nbProd * (1.2 + ($user->getPoliticProd() / 14)));
+                                        $water = $planet->getWater() + ($wtProd * (1.2 + ($user->getPoliticProd() / 14)));
+                                        $food = $planet->getFood() + ($fdProd * (1.2 + ($user->getPoliticProd() / 14)));
 
+                                    } else {
+                                        $niobium = $planet->getNiobium() + ($nbProd * 1.2);
+                                        $water = $planet->getWater() + ($wtProd * 1.2);
+                                        $food = $planet->getFood() + ($fdProd * 1.2);
+                                    }
                                 } else {
-                                    $niobium = $planet->getNiobium() + ($nbProd * 1.2);
-                                    $water = $planet->getWater() + ($wtProd * 1.2);
-                                    $food = $planet->getFood() + ($fdProd * 1.2);
+                                    $niobium = $planet->getNiobium() + $nbProd;
+                                    $water = $planet->getWater() + $wtProd;
+                                    $food = $planet->getFood() + $fdProd;
                                 }
-                            } else {
-                                $niobium = $planet->getNiobium() + $nbProd;
-                                $water = $planet->getWater() + $wtProd;
-                                $food = $planet->getFood() + $fdProd;
+                                if ($planet->getNiobiumMax() > ($planet->getNiobium() + $niobium)) {
+                                    $planet->setNiobium($niobium);
+                                } else {
+                                    $planet->setNiobium($planet->getNiobiumMax());
+                                }
+                                if ($planet->getWaterMax() > ($planet->getWater() + $water)) {
+                                    $planet->setWater($water);
+                                } else {
+                                    $planet->setWater($planet->getWaterMax());
+                                }
+                                if ($planet->getFoodMax() > ($planet->getFood() + $food)) {
+                                    $planet->setFood($food);
+                                } else {
+                                    $planet->setFood($planet->getFoodMax());
+                                }
+                                $this->em->flush($planet);
+                            } elseif ($planet->getMoon() == true) {
+                                $fdProd = ($planet->getFdProduction() * $seconds) / 60;
+                                $workerMore = (($planet->getWorkerProduction() * $workerBonus * $seconds) / 60);
+
+                                if ($seconds > 129600) {
+                                    $secondsLvl = $seconds % 86400 + 1;
+                                    $workerMore = $workerMore / $secondsLvl;
+                                    if (($planet->getWorker() + $workerMore >= $planet->getWorkerMax())) {
+                                        $planet->setWorker($planet->getWorkerMax());
+                                    } else {
+                                        $planet->setWorker($planet->getWorker() + (($planet->getWorkerProduction() * $workerBonus * $seconds) / 60));
+                                    }
+                                } else {
+                                    if (($planet->getWorker() + $workerMore >= $planet->getWorkerMax())) {
+                                        $planet->setWorker($planet->getWorkerMax());
+                                    } else {
+                                        $planet->setWorker($planet->getWorker() + (($planet->getWorkerProduction() * $workerBonus * $seconds) / 60));
+                                    }
+                                }
+
+                                if ($user->getAlly()) {
+                                    if ($user->getPoliticProd() > 0) {
+                                        $food = $planet->getFood() + ($fdProd * (1.2 + ($user->getPoliticProd() / 14)));
+
+                                    } else {
+                                        $food = $planet->getFood() + ($fdProd * 1.2);
+                                    }
+                                } else {
+                                    $food = $planet->getFood() + $fdProd;
+                                }
+                                if ($planet->getFoodMax() > ($planet->getFood() + $food)) {
+                                    $planet->setFood($food);
+                                } else {
+                                    $planet->setFood($planet->getFoodMax());
+                                }
+                                $this->em->flush($planet);
                             }
-                            if ($planet->getNiobiumMax() > ($planet->getNiobium() + $niobium)) {
-                                $planet->setNiobium($niobium);
-                            } else {
-                                $planet->setNiobium($planet->getNiobiumMax());
-                            }
-                            if ($planet->getWaterMax() > ($planet->getWater() + $water)) {
-                                $planet->setWater($water);
-                            } else {
-                                $planet->setWater($planet->getWaterMax());
-                            }
-                            if ($planet->getFoodMax() > ($planet->getFood() + $food)) {
-                                $planet->setFood($food);
-                            } else {
-                                $planet->setFood($planet->getFoodMax());
-                            }
-                            $this->em->flush($planet);
                         }
                         $user->setLastActivity($now);
                         $this->em->flush($user);
