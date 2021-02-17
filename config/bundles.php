7ad76f673edb2abd7dde881ca2e57f74658cd5b3
LeBorgne42 <rivierematthieupro@gmail.com>
2021-02-17 01:12:21 +0100

    add logs error dev and prod

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
